058f77c28f8a02d7ca34b1254398ebe37b293ad0
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-07-11 11:59:20 +0530

    Updated DTOs

---
+++ ATMAPPAPISolution/ATMAPPAPI/Models/DTOs/CardInfoRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ATMAPPAPI.Models.DTOs
+{
+    public class CardInfoRequest
+    {
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
