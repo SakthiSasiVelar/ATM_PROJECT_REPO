157dec06b2fbd3fef19bdec19aa9e941ffb9d570
Venkat S P <venkatsp2002@gmail.com>
2024-07-11 12:27:05 +0530

    $Email Service added

---
+++ ATMAPPAPISolution/ATMAPPAPI/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace ATMAPPAPI.Services
+{
+    public interface IEmailService
+    {
+        Task<string> SendOTPMail(string toEmailAddress);
+
+        Task<string> VerifyOtp(string toEmailAddress, string enteredOtp);
+    }
+}
