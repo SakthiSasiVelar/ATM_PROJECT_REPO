e929f383833d30f0c7ac57ce9396bdd9f4ad7c67
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-07-11 10:52:10 +0530

    Initial setup

---
+++ ATMAPPAPISolution/ATMAPPAPI/Contexts/AtmDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ATMAPPAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATMAPPAPI.Contexts
+{
+    public class AtmDbContext:DbContext
+    {
+        public AtmDbContext(DbContextOptions<AtmDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CardInfo> CardInfos { get; set; }
+        public DbSet<UserCredentials> UserCredentials { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
